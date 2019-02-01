63bcd9703b54b9ba05f0a7bd85e501146e9a61fd
Edy Ionescu <errorsmith@gmail.com>
2019-02-01 17:50:09 +0200

    Enable support for the experimental syntax 'dynamicImport'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
+const Warning = React.lazy(() => import('./Warning'));
+
 class App extends React.Component {
   state = {
     count: 0,
@@ -14,6 +16,11 @@ class App extends React.Component {
         <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
         <button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
         <button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
       </div>
     );
   }
