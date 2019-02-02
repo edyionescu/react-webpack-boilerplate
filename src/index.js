75c3044f5d941ee4559d1f8ddf05945ead8faa25
Edy Ionescu <errorsmith@gmail.com>
2019-02-02 18:01:04 +0200

    Create an error boundary component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import '@babel/polyfill';
+import ErrorBoundary from './ErrorBoundary';
 import App from './App';
 import './index.css';
 
 render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 );
